6b6208989dbacf4c63a34e10994601890fd03b59
Aditya-Narayan-Das <93440165+Aditya-Narayan-Das@users.noreply.github.com>
2025-07-12 09:46:58 +0530

    Update urls.py

--- project/app/urls.py
+++ project/app/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from .views import home
+from .views import chat_api
+
 urlpatterns = [
-    path('',home, name='home')
+    path('',home, name='home'),
+    path('chat/', chat_api, name='chat_api')
 ]
